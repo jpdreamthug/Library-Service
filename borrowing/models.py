8fa5ad3480407ce5d00a2690d46d5818328d90c8
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-13 18:09:42 +0300

    add constraints for dates

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import CheckConstraint, Q
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
 
@@ -14,6 +15,26 @@ class Borrowing(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
     )
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check=Q(borrow_date__gte=timezone.now().date()),
+                name="borrow_date_not_in_past"
+            ),
+            CheckConstraint(
+                check=Q(expected_return_date__gt=models.F("borrow_date")),
+                name="expected_return_after_borrow"
+            ),
+            CheckConstraint(
+                check=Q(actual_return_date__gte=models.F("borrow_date")) | Q(actual_return_date__isnull=True),
+                name="actual_return_after_borrow_or_null"
+            ),
+            CheckConstraint(
+                check=Q(actual_return_date__lte=models.F("expected_return_date")) | Q(actual_return_date__isnull=True),
+                name="actual_return_before_expected_or_null"
+            ),
+        ]
 
     @staticmethod
     def validate_dates(borrow_date, expected_return_date, actual_return_date, error):
