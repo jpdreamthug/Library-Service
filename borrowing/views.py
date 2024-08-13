f080fe42549eb537628ffb5237d8185d52d1c519
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-13 21:12:16 +0300

    add create serializer, validation and attach current user to the object

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, mixins
+from rest_framework.exceptions import PermissionDenied
 
 from borrowing.mixins import GenericMethodsMixin
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
     BorrowingListSerializer,
-    BorrowingDetailSerializer,
+    BorrowingDetailSerializer, BorrowingCreateSerializer,
 )
 
 
@@ -21,4 +22,10 @@ class BorrowingViewSet(
     action_serializers = {
         "list": BorrowingListSerializer,
         "retrieve": BorrowingDetailSerializer,
+        "create": BorrowingCreateSerializer,
     }
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("Authentication credentials were not provided.")
+        serializer.save(user=self.request.user)
