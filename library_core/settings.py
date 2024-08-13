875cf8690744fbbd9f778998c112646f66f1ea69
Sergii Musiienko <vpnmus@ukr.net>
2024-08-13 14:40:23 +0300

    Change AUTH_HEADER_NAME 'Authorization' on 'Authorize' for JWT

--- library_core/settings.py
+++ library_core/settings.py
@@ -143,6 +143,7 @@ SIMPLE_JWT = {
     "SIGNING_KEY": SECRET_KEY,
     "VERIFYING_KEY": None,
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "Authorize",
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
