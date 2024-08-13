1ef0948e56edc2862c2150cd0b81679b41c01339
Ivan Vahanov <ivan.seeker6@gmail.com>
2024-08-13 14:25:22 +0300

    update urls

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
+app_name = "book"
+
 router = DefaultRouter()
 router.register(r"books", BookViewSet, basename="book")
 
