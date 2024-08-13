7b641b831c761c5e28bf1f402b8b5b7bd6f4c00d
Sergii Musiienko <vpnmus@ukr.net>
2024-08-13 14:27:26 +0300

    Add in serializer two fields for display

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "first_name", "last_name", "is_staff")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
