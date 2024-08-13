6c174a923405e3e4d73b113de358594edccf2dc9
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-13 17:53:50 +0300

    optimize queryset, fix imports style and register in admin

--- borrowing/admin.py
+++ borrowing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from borrowing.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    ...
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets, mixins
-from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingDetailSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer
+)
 from borrowing.mixins import GenericMethodsMixin
 
 
@@ -13,11 +16,9 @@ class BorrowingViewSet(
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet,
 ):
-    queryset = Borrowing.objects.all()
+    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingSerializer
     action_serializers = {
         "list": BorrowingListSerializer,
         "retrieve": BorrowingDetailSerializer
     }
-    permission_classes = (IsAuthenticated,)
-
