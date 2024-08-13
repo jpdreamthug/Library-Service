6bb33165cc80b3e3dcbcd187e287eda3900eeabd
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-13 18:15:31 +0300

    remove commented row

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from borrowing.views import BorrowingViewSet
 
 borrowing_list_create = BorrowingViewSet.as_view({'get': 'list', "post": "create"})
 borrowing_detail = BorrowingViewSet.as_view({'get': 'retrieve'})
-# borrowing_create = BorrowingViewSet.as_view({'post': 'create'})
 
 urlpatterns = [
     path("", borrowing_list_create, name="borrowing-list"),
