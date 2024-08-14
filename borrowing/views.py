5ccdf3ef50335e34402a03f65ba934746adb924a
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-14 11:51:10 +0300

    add filtering by is active borrowing and user id for admins

--- borrowing/views.py
+++ borrowing/views.py
@@ -26,6 +26,23 @@ class BorrowingViewSet(
         "create": BorrowingCreateSerializer,
     }
 
+    def get_queryset(self):
+        queryset = self.queryset
+        is_admin = self.request.user.is_staff or self.request.user.is_superuser
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+
+        if user_id and is_admin:
+            queryset = queryset.filter(user_id=user_id)
+
+        if not is_admin:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset
+
     def perform_create(self, serializer):
         if not self.request.user.is_authenticated:
             raise PermissionDenied("Authentication credentials were not provided.")
