dcba00430beef97bc3ad36420afaf15915218ccc
AndrewwSS <clash2clans1one@gmail.com>
2024-08-15 14:50:34 +0300

    merge create_stripe functions to one function

--- payment/services.py
+++ payment/services.py
@@ -10,10 +10,19 @@ from payment.models import Payment
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-def create_stripe_payment_session(
-    borrowing: Borrowing, request: HttpRequest
+def create_payment_session(
+        borrowing: Borrowing,
+        request: HttpRequest,
+        payment_type: Payment.Type,
+        save=True
 ) -> Payment:
-    total_price = borrowing.get_payment_amount()
+
+    if payment_type == Payment.Type.PAYMENT:
+        total_price = borrowing.get_payment_amount()
+        product_name = borrowing.book.title
+    else:
+        total_price = borrowing.get_fine_amount()
+        product_name = f"Fine for {borrowing.book.title}"
 
     success_url = request.build_absolute_uri(settings.PAYMENT_SUCCESS_URL)
     cancel_url = request.build_absolute_uri(settings.PAYMENT_CANCEL_URL)
@@ -25,7 +34,7 @@ def create_stripe_payment_session(
                 "price_data": {
                     "currency": "usd",
                     "product_data": {
-                        "name": borrowing.book.title,
+                        "name": product_name,
                     },
                     "unit_amount": total_price,
                 },
@@ -37,47 +46,14 @@ def create_stripe_payment_session(
         cancel_url=cancel_url,
     )
 
-    payment = Payment.objects.create(
+    payment = Payment(
         borrowing=borrowing,
         session_url=session.url,
         session_id=session.id,
         money_to_pay=total_price,
+        type=payment_type
     )
-
-    return payment
-
-
-def create_stripe_fine_session(borrowing: Borrowing, request: HttpRequest) -> Payment:
-    fine_amount = borrowing.get_fine_amount()
-
-    success_url = request.build_absolute_uri(settings.PAYMENT_SUCCESS_URL)
-    cancel_url = request.build_absolute_uri(settings.PAYMENT_CANCEL_URL)
-
-    session = stripe.checkout.Session.create(
-        payment_method_types=["card"],
-        line_items=[
-            {
-                "price_data": {
-                    "currency": "usd",
-                    "product_data": {
-                        "name": f"Fine for borrowing book - {borrowing.book.title}",
-                    },
-                    "unit_amount": fine_amount,
-                },
-                "quantity": 1,
-            }
-        ],
-        mode="payment",
-        success_url=success_url + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url=cancel_url,
-    )
-
-    payment = Payment.objects.create(
-        borrowing=borrowing,
-        session_url=session.url,
-        session_id=session.id,
-        money_to_pay=fine_amount,
-        type=Payment.Type.FINE,
-    )
+    if save:
+        payment.save()
 
     return payment
