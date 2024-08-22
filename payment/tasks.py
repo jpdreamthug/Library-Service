e1a9e19480c362625dad95055a46bb22fcd16fbd
AndrewwSS <clash2clans1one@gmail.com>
2024-08-22 10:42:11 +0300

    add exception handling for stripe retrieve session

--- payment/tasks.py
+++ payment/tasks.py
@@ -7,11 +7,19 @@ from payment.models import Payment
 
 @shared_task
 def check_payment_expiration() -> None:
-    payments = Payment.objects.all()
+    payments = Payment.objects.filter(
+        status="PENDING"
+    )
     for payment in payments:
-        session = stripe.checkout.Session.retrieve(
-            payment.session_id
-        )
+        try:
+            session = stripe.checkout.Session.retrieve(
+                payment.session_id
+            )
+        except stripe.error.InvalidRequestError as e:
+            print(f"Invalid session: {e}")
+        except Exception as e:
+            print(e)
+
         if session.status == "expired":
             print(f"Found expired session: {session.id}")
             payment.status = Payment.Status.EXPIRED
