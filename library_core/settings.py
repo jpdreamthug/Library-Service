45748b37468048c3ed9a718a77ea1c54ff97c56a
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-27 09:59:30 +0300

    add default pagination

--- library_core/settings.py
+++ library_core/settings.py
@@ -145,7 +145,10 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
+
 SPECTACULAR_SETTINGS = {
     "TITLE": "Library API documentation",
     "DESCRIPTION": "Mate academy group task",
